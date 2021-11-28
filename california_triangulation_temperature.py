1a4a4cb6b5e958ad06aff58ad9201e2a00aa63c8
scarlettfan <scarlettfan0108@gmail.com>
2021-11-28 00:08:48 -0800

    changed colormap system  and add heatmap but heatmap need to be improved

--- california_triangulation_temperature.py
+++ california_triangulation_temperature.py
@@ -11,14 +11,16 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input
 from functools import reduce
+import seaborn as sns
 
 app = dash.Dash(__name__)
+
 
 app.layout = html.Div(
     children=[
         html.Div(
             children=[
-                #html.P(children="🥑", className="header-emoji"),
+                # html.P(children="🥑", className="header-emoji"),
                 html.P(children="🥑", className="header-emoji"),
                 html.H1(
                     children="Triangulated California Weather Map For Years 2010-2020", className="header-title"
@@ -32,17 +34,18 @@ app.layout = html.Div(
         ),
         html.Div(
             children=[
-               html.Div(
+                html.Div(
                     children=[
-                        html.Div(children="Weather Type", className="menu-title"),
+                        html.Div(children="Weather Type",
+                                 className="menu-title"),
                         dcc.Dropdown(
                             id="weather-type-filter",
                             options=[
-                                {"label": "Temperature", "value": "Temperature"}],
-                                # {"label": "Precipitation", "value": "Precipitation"}],
+                               {"label": "Temperature", "value": "Temperature"}],
+                            # {"label": "Precipitation", "value": "Precipitation"}],
                             clearable=False,
                             searchable=False,
-                            value="Temperature",                       
+                            value="Temperature",
                             className="dropdown",
                         ),
                     ],
@@ -53,7 +56,7 @@ app.layout = html.Div(
                         html.Div(
                             children="Year",
                             className="year-slider-title"
-                            ),
+                        ),
                         dcc.Slider(
                             id='year-slider-filter',
                             min=2010,
@@ -61,29 +64,30 @@ app.layout = html.Div(
                             step=1,
                             value=2010,
                             className="myslider",
-                                marks={
+                            marks={
                                 2010: '2010',
                                 2011: '2011',
                                 2012: '2012',
                                 2013: '2013',
                                 2014: '2014',
-                                2015: '2015',                               
+                                2015: '2015',
                                 2016: '2016',
                                 2017: '2017',
                                 2018: '2018',
                                 2019: '2019',
                                 2020: '2020',
                             },
-                            tooltip={"placement": "bottom", "always_visible": False},
+                            tooltip={"placement": "bottom",
+                                     "always_visible": False},
                         ),
                     ]
                 ),
-               html.Div(
+                html.Div(
                     children=[
                         html.Div(
                             children="Month",
                             className="month-slider-title"
-                            ),
+                        ),
                         dcc.Slider(
                             id='month-slider-filter',
                             min=1,
@@ -91,13 +95,13 @@ app.layout = html.Div(
                             step=1,
                             value=1,
                             className="myslider",
-                                marks={
+                            marks={
                                 1: 'JAN',
                                 2: 'FEB',
                                 3: 'MAR',
                                 4: 'APR',
                                 5: 'MAY',
-                                6: 'JUN',                               
+                                6: 'JUN',
                                 7: 'JUL',
                                 8: 'AUG',
                                 9: 'SEP',
@@ -105,7 +109,8 @@ app.layout = html.Div(
                                 11: 'NOV',
                                 12: 'DEC'
                             },
-                            tooltip={"placement": "bottom", "always_visible": False},
+                            tooltip={"placement": "bottom",
+                                     "always_visible": False},
                         ),
                     ]
                 ),
@@ -120,12 +125,10 @@ app.layout = html.Div(
 )
 
 
-
-
 #  The following commented code is used to read raw data and clean up missing data using the following logic:
 #  1) Skip station that has 132 month of data (11 years)
 #  2) Skip station that has more than 30 missing TAVG values
-#  3) For each station, calculate the average value for the same month from other years which have values for that same year. 
+#  3) For each station, calculate the average value for the same month from other years which have values for that same year.
 #      using this average value to fill other missing data for this station for the same month
 #  Finally write the data into csv file cal_2010_2020.csv
 
@@ -163,7 +166,7 @@ app.layout = html.Div(
 #     if (len(where_list[0]) <= 30):
 #         date_data = list(goodstation_data["DATE"])
 #         tavg_data = goodstation_data["TAVG"]
-#         station_monthly_tavg = [round(np.mean([item for item in tavg_data[np.where(np.char.find(date_data,month)>=0)[0]] 
+#         station_monthly_tavg = [round(np.mean([item for item in tavg_data[np.where(np.char.find(date_data,month)>=0)[0]]
 #                             if np.isnan(item) == False]),1) for month in months]
 #         for pos in where_list[0]:
 #             goodstation_data["TAVG"][pos] = station_monthly_tavg[pos % 12]
@@ -174,7 +177,8 @@ app.layout = html.Div(
 # df_raw_10y.to_csv('cal_2010_2020.csv', index=False)
 
 
-months = ["-01","-02","-03","-04","-05","-06","-07","-08","-09","-10","-11","-12"]
+months = ["-01", "-02", "-03", "-04", "-05", "-06",
+          "-07", "-08", "-09", "-10", "-11", "-12"]
 
 df_raw = pd.read_csv("cal_2010_2020.csv")
 df_jan = df_raw[df_raw["DATE"] == "2010-01"]
@@ -183,130 +187,181 @@ y_cal_lat = df_jan['LATITUDE'].tolist()
 z_cal = [0] * len(y_cal_lat)
 
 
-points2D_cal=np.vstack([x_cal_long,y_cal_lat]).T
+points2D_cal = np.vstack([x_cal_long, y_cal_lat]).T
 tri_cal = Delaunay(points2D_cal)
 
 
-sp1_index=np.where(tri_cal.points == [-120.18,41.99])[0][1]  # 354
-sp2_index=np.where(tri_cal.points == [-118.358,37.3711])[0][1] #202
-sp3_index=np.where(tri_cal.points == [-117.1449,36.602])[0][1] # 80
-sp4_index=np.where(tri_cal.points == [-116.8672,36.4626])[0][1]  # 130
-sp5_index=np.where(tri_cal.points == [-115.9092,35.7706])[0][1]  # 389
-sp6_index=np.where(tri_cal.points == [-114.6188,34.7675])[0][1]  #66
-sp7_index=np.where(tri_cal.points == [-119.37,38.44])[0][1]  #213
-s_1 = {sp1_index,sp2_index,sp3_index}
-s_2 = {sp1_index,sp3_index,sp4_index}
-s_3 = {sp1_index,sp4_index,sp5_index}
-s_4 = {sp1_index,sp5_index,sp6_index}
-s_5 = {sp1_index,sp2_index,sp7_index}
+sp1_index = np.where(tri_cal.points == [-120.18, 41.99])[0][1]  # 354
+sp2_index = np.where(tri_cal.points == [-118.358, 37.3711])[0][1]  # 202
+sp3_index = np.where(tri_cal.points == [-117.1449, 36.602])[0][1]  # 80
+sp4_index = np.where(tri_cal.points == [-116.8672, 36.4626])[0][1]  # 130
+sp5_index = np.where(tri_cal.points == [-115.9092, 35.7706])[0][1]  # 389
+sp6_index = np.where(tri_cal.points == [-114.6188, 34.7675])[0][1]  # 66
+sp7_index = np.where(tri_cal.points == [-119.37, 38.44])[0][1]  # 213
+s_1 = {sp1_index, sp2_index, sp3_index}
+s_2 = {sp1_index, sp3_index, sp4_index}
+s_3 = {sp1_index, sp4_index, sp5_index}
+s_4 = {sp1_index, sp5_index, sp6_index}
+s_5 = {sp1_index, sp2_index, sp7_index}
 
-tri_cal.simplices = np.array([item for item in tri_cal.simplices 
-                                if (set(item) == s_1 or 
-                                     set(item) == s_2 or 
-                                     set(item) == s_3 or 
-                                     set(item) == s_4 or 
-                                     set(item) == s_5) == False])
+tri_cal.simplices = np.array([item for item in tri_cal.simplices
+                              if (set(item) == s_1 or
+                                  set(item) == s_2 or
+                                  set(item) == s_3 or
+                                  set(item) == s_4 or
+                                  set(item) == s_5) == False])
 
 s_year_global = 2010
 
+t_global = []
+
+
 def map_z2color(zval, colormap, vmin, vmax):
-    #map the normalized value zval to a corresponding color in the colormap
-    if vmin>vmax:
+    # map the normalized value zval to a corresponding color in the colormap
+    if vmin > vmax:
         raise ValueError('incorrect relation between vmin and vmax')
-    t=(zval-vmin)/float((vmax-vmin))#normalize val
-    R, G, B, alpha=colormap(t)
-    return 'rgb('+'{:d}'.format(int(R*255+0.5))+','+'{:d}'.format(int(G*255+0.5))+\
+    t = (zval-vmin)/float((vmax-vmin))  # normalize val
+    t_global.append(t)
+    print("len: ", len(t_global))
+    R, G, B, alpha = colormap(t)
+    return 'rgb('+'{:d}'.format(int(R*255+0.5))+','+'{:d}'.format(int(G*255+0.5)) +\
            ','+'{:d}'.format(int(B*255+0.5))+')'
 
 
 def tri_indices(simplices):
-    #simplices is a numpy array defining the simplices of the triangularization
-    #returns the lists of indices i, j, k
-   return ([triplet[c] for triplet in simplices] for c in range(3))
+    # simplices is a numpy array defining the simplices of the triangularization
+    # returns the lists of indices i, j, k
+    return ([triplet[c] for triplet in simplices] for c in range(3))
 
 
+def plotly_trisurf_cal(x, y, z, z_temperature, simplices, colormap=cm.coolwarm, plot_edges=None):
+    # x, y, z are lists of coordinates of the triangle vertices
+    # simplices are the simplices that define the triangularization;
+    # simplices  is a numpy array of shape (no_triangles, 3)
+    # insert here the  type check for input data
+    t_global.clear()
+    points3D = np.vstack((x, y, z_temperature)).T
+    # vertices of the surface triangles
+    tri_vertices = list(map(lambda index: points3D[index], simplices))
+    # mean values of z-coordinates of triangle vertices
+    zmean = [np.mean(tri[:, 2]) for tri in tri_vertices]
+    min_zmean = np.min(zmean)
+    max_zmean = np.max(zmean)
+    facecolor = [map_z2color(zz,  colormap, min_zmean, max_zmean)
+                 for zz in zmean]
+    print("t_global_len: ", len(t_global))
 
-
-def plotly_trisurf_cal(x, y, z, z_temperature, simplices, colormap=cm.RdBu, plot_edges=None):
-    #x, y, z are lists of coordinates of the triangle vertices 
-    #simplices are the simplices that define the triangularization;
-    #simplices  is a numpy array of shape (no_triangles, 3)
-    #insert here the  type check for input data
-
-    points3D=np.vstack((x,y,z_temperature)).T
-    tri_vertices=list(map(lambda index: points3D[index], simplices))# vertices of the surface triangles
-    zmean=[np.mean(tri[:,2]) for tri in tri_vertices]  # mean values of z-coordinates of triangle vertices
-    min_zmean=np.min(zmean)
-    max_zmean=np.max(zmean)
-    facecolor=[map_z2color(zz,  colormap, min_zmean, max_zmean) for zz in zmean]
-
-    I,J,K=tri_indices(simplices)
-    triangles=go.Mesh3d(x=x,
-                     y=y,
-                     z=z,
-                     facecolor=facecolor,
-                     i=I,
-                     j=J,
-                     k=K,
-                     name=''
-                    )
-    if plot_edges is None:# the triangle sides are not plotted 
+    I, J, K = tri_indices(simplices)
+    triangles = go.Mesh3d(x=x,
+                          y=y,
+                          z=z,
+                          facecolor=facecolor,
+                          i=I,
+                          j=J,
+                          k=K,
+                          name=''
+                          )
+    if plot_edges is None:  # the triangle sides are not plotted
         return [triangles]
     else:
-        #define the lists Xe, Ye, Ze, of x, y, resp z coordinates of edge end points for each triangle
-        #None separates data corresponding to two consecutive triangles
-        points3D=np.vstack((x,y,z)).T
-        tri_vertices=list(map(lambda index: points3D[index], simplices))# vertices of the surface triangles
-        lists_coord=[[[T[k%3][c] for k in range(4)]+[ None]   for T in tri_vertices]  for c in range(3)]
-        Xe, Ye, Ze=[reduce(lambda x,y: x+y, lists_coord[k]) for k in range(3)]
+        # define the lists Xe, Ye, Ze, of x, y, resp z coordinates of edge end points for each triangle
+        # None separates data corresponding to two consecutive triangles
+        points3D = np.vstack((x, y, z)).T
+        # vertices of the surface triangles
+        tri_vertices = list(map(lambda index: points3D[index], simplices))
+        lists_coord = [[[T[k % 3][c] for k in range(
+            4)]+[None] for T in tri_vertices] for c in range(3)]
+        Xe, Ye, Ze = [reduce(lambda x, y: x+y, lists_coord[k])
+                      for k in range(3)]
 
-        #define the lines to be plotted
-        lines=go.Scatter3d(x=Xe,
-                        y=Ye,
-                        z=Ze,
-                        mode='lines',
-                        line=dict(color= 'rgb(50,50,50)', width=1.5)
-               )
+        # define the lines to be plotted
+        lines = go.Scatter3d(x=Xe,
+                             y=Ye,
+                             z=Ze,
+                             mode='lines',
+                             line=dict(color='rgb(50,50,50)', width=1.5)
+                             )
         return [triangles, lines]
 
 
-noaxis=dict(showbackground=False,
-            showline=False,
-            zeroline=False,
-            showgrid=False,
-            showticklabels=False,
-            title=''
-          )
-layout = go.Layout(
-         title='Triangulated California Weather Map',
-         width=1200,
-         height=1200,
-         scene=dict(xaxis=noaxis,
-                    yaxis=noaxis,
-                    zaxis=noaxis,
-                    aspectratio=dict(x=10, y=10, z=0),
-                    camera=dict(eye=dict(x=0.001,
-                                     y=-0.015,
-                                     z= 1.2)
-                            )
-                )
-        )
+def generateDiscreteColourScale(colour_set):
+    # colour set is a list of lists
+    colour_output = []
+    num_colours = len(colour_set)
+    divisions = 1./num_colours
+    c_index = 0.
+    # Loop over the colour set
+    for cset in colour_set:
+        num_subs = len(cset)
+        sub_divisions = divisions/num_subs
+        # Loop over the sub colours in this set
+        for subcset in cset:
+            colour_output.append((c_index, subcset))
+            colour_output.append((c_index + sub_divisions -
+                                  .001, subcset))
+            c_index = c_index + sub_divisions
+    colour_output[-1] = (1, colour_output[-1][1])
+    return colour_output
 
-@app.callback(
+
+noaxis = dict(showbackground=False,
+              showline=False,
+              zeroline=False,
+              showgrid=False,
+              showticklabels=False,
+              title=''
+              )
+layout = go.Layout(
+    title='Triangulated California Weather Map',
+    width=1200,
+    height=1200,
+    scene=dict(xaxis=noaxis,
+               yaxis=noaxis,
+               zaxis=noaxis,
+               aspectratio=dict(x=10, y=10, z=0),
+               camera=dict(eye=dict(x=0.001,
+                                    y=-0.015,
+                                    z=1.2)
+                           )
+               )
+)
+data = px.data.gapminder()
+
+data_canada = data[data.country == 'Canada']
+
+
+@ app.callback(
     Output(component_id='cal_3d_obj', component_property='figure'),
+
     [
         Input("weather-type-filter", "value"),
         Input("year-slider-filter", "value"),
-        Input("month-slider-filter", "value"),
+        Input("month-slider-filter", "value")
     ],
 
 
 )
-
-def update_graph(weather_type,s_year,s_month):
-    z_cal_temp = (df_raw[df_raw["DATE"] == str(s_year)+str(months[s_month-1])])["TAVG"]
-    graph_data=plotly_trisurf_cal(x_cal_long,y_cal_lat, z_cal, z_cal_temp, tri_cal.simplices, colormap=cm.cubehelix, plot_edges=True)
+def update_graph(weather_type, s_year, s_month):
+    z_cal_temp = (df_raw[df_raw["DATE"] == str(
+        s_year)+str(months[s_month-1])])["TAVG"]
+    graph_data = plotly_trisurf_cal(
+        x_cal_long, y_cal_lat, z_cal, z_cal_temp, tri_cal.simplices, colormap=cm.coolwarm, plot_edges=True)
     fig = go.Figure(data=graph_data, layout=layout)
+    # print(fig.data[0]['facecolor'])
+    print("377 len:", len(t_global))
+    fig.add_trace(go.Heatmap(
+        z=[t_global],
+        colorscale=[
+            [1, 'rgb(250, 250, 250)'],  # 0
+            [0, 'rgb(0, 0, 0)']
+        ],
+        colorbar=dict(
+            tick0=0,
+            tickmode='array',
+            tickvals=[0, 1000, 10000, 100000],
+            dtick=1
+        )
+    ))
     return fig
 
 
